8615eb767fa711cc3d06e6b0981668dd9b181d98
Thomas Pappas <getekid@gmail.com>
2023-10-30 20:43:40 +0200

    Student refactoring

--- src/models.py
+++ src/models.py
@@ -1,8 +1,10 @@
 import torch
+import numpy as np
 from torch import nn
 from torch.nn.functional import one_hot
 from torch.distributions import Dirichlet
 from src.ebmUtils import Reg, RegLatent, loss_functional
+import matplotlib.pyplot as plt
 
 
 class LVGEBM(nn.Module):
@@ -236,6 +238,119 @@ class Voronoi(nn.Module):
 
         )
 
+        # Store training variables.
+        self.best_vor_model_state = None
+        self.cost_ll = None
+        self.acc_l = None
+        self.es = None
+
     def forward(self, x):
         x = self.predictor(x)
         return x
+
+    def train_(self,
+               optimizer,
+               epochs,
+               device,
+               qp,
+               F_ps):
+        """
+            Train the student model
+
+            Parameters:
+                optimizer: optimizer to be used
+                epochs: number of epochs
+                device: device to be used
+                qp: qp points
+                F_ps: ???
+        """
+        print("Training Teacher Model")
+        F_l = []
+        z_l = []
+        cost_l = []
+        cost_ll = []
+        qp = torch.tensor(qp).to(device)
+        outputs = torch.zeros(qp.shape[0], self.n_centroids)
+
+        ce = nn.CrossEntropyLoss()
+        acc_l = []
+        l_ce = []
+        es = []
+        best_vor_cost = torch.inf
+        best_vor_model_state = None
+        for epoch in range(epochs):
+            qp = torch.tensor(qp, dtype=torch.float32).to(device)
+            # get outputs
+            outputs = self(qp)
+            # pass outputs through a hard arg max
+            F, z = outputs.min(1)
+            F_ps_m, z_ps_m = F_ps.min(1)
+            # send to device
+            F_ps_m = F_ps_m.to(device)
+            z_ps_m = z_ps_m.to(device)
+            # make values of z_ps_m to be round to nearest integer
+            r_z = torch.round(z_ps_m)
+            # get loss
+            alpha = 100
+            beta = 1000
+            z = z.float()
+            z_ = outputs
+            # make z_ float
+            z_ = z_.float()
+            z_cost = ce(z_, z_ps_m)
+            # get z_cost where we only penalize the wrong z and not its distance
+            # z_cost = penalty(z, r_z)
+            # F_l.append(F_cost.item())
+            z_l.append(z_cost)
+            # cost = alpha*F_cost + beta*z_cost
+            cost = 100 * z_cost
+            cost_l.append(cost.item())
+            # backward
+            optimizer.zero_grad()
+            cost.backward()
+            optimizer.step()
+            if cost < best_vor_cost:
+                best_vor_cost = cost
+                best_vor_model_state = self.state_dict()
+            if epoch % 2000 == 0:
+                # lets check acc
+                acc = 0
+                for i in range(qp.shape[0]):
+                    F_e, z_e = outputs[i].max(0)
+                    if z_e == z_ps_m[i]:
+                        acc += 1
+                acc_l.append(acc / qp.shape[0])
+                es.append(epoch)
+                cost_ll.append(cost.item())
+
+                if epoch % 200 == 0:
+                    print("Acc: ", acc / qp.shape[0])
+                    print("Epoch: ", epoch, "Cost: ", cost.item())
+
+        # Store the training variables to the model.
+        self.best_vor_model_state = best_vor_model_state
+        self.cost_ll = cost_ll
+        self.acc_l = acc_l
+        self.es = es
+
+    def plot_accuracy_and_loss(self):
+        cost_ll_log = np.log(self.cost_ll)
+
+        fig, ax1 = plt.subplots(figsize=(10, 10))
+        ax1.plot(self.es, self.acc_l, c='r', label='Accuracy', linestyle='dotted', linewidth=3)
+        ax1.set_xlabel('Epochs')
+        ax1.set_ylabel('Accuracy')
+        ax1.legend(loc='upper left')
+        ax1.set_ylim([0, 1])
+        # create another twin axis
+        ax2 = ax1.twinx()
+        ax2.plot(self.es, cost_ll_log, c='royalblue', label='Loss', linestyle='dashed', linewidth=3)
+        ax2.set_ylabel('Log Loss')
+        ax2.legend(loc='upper right')
+        ax2.set_ylim([-1, 6])
+        # set x_lim in both
+        ax1.set_xlim([0, self.epochs])
+        ax2.set_xlim([0, self.epochs])
+        # add stars to the plot points
+        ax1.scatter(self.es, self.acc_l, c='r', s=100, marker='*')
+        ax2.scatter(self.es, cost_ll_log, c='royalblue', s=100, marker='*')
