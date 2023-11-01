3e731e987157345a0ed04d6ae937175cb5ef9c62
Thomas Pappas <getekid@gmail.com>
2023-11-02 00:45:53 +0200

    Adds plots after teacher training, uncertainty area calculation, labeling and student training
    Also removes student build from quadtree init to have more control over the arguments

--- src/quadtree.py
+++ src/quadtree.py
@@ -2,6 +2,10 @@ import torch
 import numpy as np
 from src.models import LVGEBM, Voronoi
 from src.utils.functions import getUncertaintyArea, getE, NearestNeighbour
+import math as m
+import matplotlib.pyplot as plt
+from src.utils import plot_tools as pt
+
 
 class QuadTree:
     """Implements a quadtree class to use in Hierarchical Clustering"""
@@ -12,7 +16,7 @@ class QuadTree:
         self.data = data                #The input data (objects)
         self.divided = False
         self.root = self.Node(self.data, 0, self)
-        self.root.create_student(teacher_args, un_args, student_args)   #Extract the trained student model for the root node
+        # self.root.create_student(teacher_args, un_args, student_args)   #Extract the trained student model for the root node
 
     class Node:
         """Implements a node class to use in a tree."""
@@ -28,50 +32,89 @@ class QuadTree:
             self.best_z = torch.empty(0)
 
         def create_student(self, teacher_args, un_args, student_args, plot=False):
+            # First calculate the area of the data.
+            x_lim = [m.floor(min(self.data[:, 0])), m.ceil(max(self.data[:, 0]))]
+            y_lim = [m.floor(min(self.data[:, 1])), m.ceil(max(self.data[:, 1]))]
+
             # Create and train the teacher model.
             teacher = LVGEBM(4, 2, 400).to(self.device)
             # Populate the optimizer with the model parameters and the learning rate.
             teacher_args["optimizer"] = torch.optim.Adam(teacher.parameters(), lr=teacher_args["optimizer_lr"])
             del teacher_args["optimizer_lr"]
+            # Train the teacher model and assign the best state found during training.
             teacher.train_(**teacher_args)
-            # Get best outputs.
-            best_outputs = teacher(teacher.z)
-            # TODO: Save model and plot training data.
+            # Plot training results.
+            if plot:
+                # Plot the Amplitude demodulation of the signal (costs array).
+                signal = teacher.cost_array
+                upper_signal, lower_signal, filtered_signal = pt.AM_dem(signal, fc=0.4 * len(signal),
+                                                                        fs=2 * len(signal))
+                pt.plot_AM_dem(upper_signal, lower_signal, filtered_signal, signal, teacher.best_epoch)
+                # Plot the best model with the best outputs.
+                manifold = pt.createManifold(teacher, teacher.best_outputs.cpu())
+                pt.plotManifold(self.data, manifold, teacher.best_outputs.cpu(), x_lim, y_lim)
+                plt.show()
 
-            # Uncertainty Area.
-            m_points = getUncertaintyArea(outputs=best_outputs.cpu().detach().numpy(), **un_args)
+            # Calculate the Uncertainty Area.
+            m_points = getUncertaintyArea(outputs=teacher.best_outputs.cpu().detach().numpy(),
+                                          x_area=x_lim, y_area=y_lim, model=None, **un_args)
             m_points = np.array(m_points)
+            # Plot the Uncertainty Area.
+            if plot:
+                # Plot the m points that are in the uncertainty area.
+                fig = plt.figure(figsize=(10, 10))
+                ax = fig.add_subplot(111)
+                ax.scatter(m_points[:, 0], m_points[:, 1], s=20, c='royalblue',
+                           alpha=0.5, marker='*', label='Uncertainty Area')
+                ax.contourf(manifold[:, :, 0], manifold[:, :, 1], manifold[:, :, 2],
+                            levels=200, cmap='viridis', alpha=0.2)
+                plt.legend()
+                plt.show()
 
             # Labeling.
             qp = np.random.permutation(m_points)
             qp = torch.tensor(qp)
-            
-            # TODO: Pass squares to getE as an argument.
             F, z, F_sq, z_sq = getE(teacher, teacher.best_outputs.cpu(), qp, self.data)
-            # TODO: Plot Uncertainty Area.
-
             # Initialize the pseudo clusters.
             # Append data that their z_sq is i for each centroid.
             pseudo_clusters = [self.data[z_sq == i] for i in range(teacher.n_centroids)]
-
             # Create labels.
             outputs_shape = (qp.shape[0], teacher.n_centroids)
             F_ps = torch.zeros(outputs_shape)
             z_ps = torch.zeros(outputs_shape)
             for i in range(outputs_shape[0]):
+                if i % 1000 == 0:
+                    print(f"Labeled {i}/{outputs_shape[0]} points.")
                 for j in range(outputs_shape[1]):
                     qpoint = qp[i].cpu().detach().numpy()
                     F_ps[i, j], z_ps[i, j] = torch.tensor(NearestNeighbour(qpoint, pseudo_clusters[j]))
-            # TODO: Plot labels.
+            print(f"Labeled all {outputs_shape[0]}/{outputs_shape[0]} points.")
+            # Plot the labels.
+            if plot:
+                # plot qp
+                fig = plt.figure(figsize=(10, 10))
+                ax = fig.add_subplot(111)
+                plt_qp = qp.cpu().detach().numpy()
+                new_labels = F_ps.min(1)[1].cpu().detach().numpy()
+                ax.scatter(plt_qp[:, 0], plt_qp[:, 1], c=new_labels, s=50)
+                # plot best_outputs
+                plt_bo = teacher.best_outputs.cpu().detach().numpy()
+                c = np.linspace(0, plt_bo.shape[0], plt_bo.shape[0])
+                ax.scatter(plt_bo[:, 0], plt_bo[:, 1], c=c, s=200)
+                pt.plot_data_on_manifold(fig, ax, self.data, size=10, limits=x_lim + y_lim)
+                plt.show()
 
-            # Create and train the student model.
+            # Create and train the student model and assign the best state found during training.
             student = Voronoi(4, 2, 2).to(self.device)  # initialize the voronoi network
             student.train_(optimizer=torch.optim.Adam(student.parameters(), lr=student_args["optimizer_lr"]),
-                        epochs=student_args["epochs"],
-                        device=self.device,
-                        qp=torch.tensor(qp),
-                        F_ps=F_ps)
-            # TODO: Save model and plot training data.
+                           epochs=student_args["epochs"],
+                           device=self.device,
+                           qp=qp,
+                           F_ps=F_ps)
+            # Plot the student training results.
+            if plot:
+                student.plot_accuracy_and_loss(student_args["epochs"])
+                plt.show()
 
             self.student = student
             self.best_z = teacher.best_z
