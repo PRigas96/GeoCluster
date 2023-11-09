d84da3ee11a0390987922dd4de7b4e84bb5d69de
Minas <minas.dsp@gmail.com>
2023-11-09 21:21:40 +0200

    Implemented functionality for 3d data generation (to be tested)

--- src/geometry.py
+++ src/geometry.py
@@ -85,6 +85,45 @@ def create_square2(square):
 
     return vertices
 
+def create_cuboid(cuboid):
+    """
+        Create a cuboid given its center of mass, edge sizes and rotations
+
+        Parameters:
+            square (list): [x_center, y_center, z_center, width, height, depth, theta, psi, phi]
+
+        Returns:
+            np.array: 8x3 array of the vertices of the cuboid
+    """
+
+    #Get the data of the cuboid
+    x_center = cuboid[0]
+    y_center = cuboid[1]
+    z_center = cuboid[2]
+    mass_center = np.array([x_center, y_center, z_center])
+    width = cuboid[3]
+    height = cuboid[4]
+    depth = cuboid[5]
+    theta = cuboid[6]
+    psi = cuboid[7]
+    phi = cuboid[8]
+
+    # Calculate the coordinates of the vertices as if the cuboid is not rotated
+    vertices = np.zeros((8, 3))
+    offsets = np.array([[1, 1, 1], [1, -1, 1], [-1, -1, 1], [-1, 1, 1], [1, 1, -1], [1, -1, -1], [-1, -1, -1], [-1, 1, -1]])
+
+    for i in range(8):
+        vertices[i] = mass_center + np.multiply(offsets[i], [width / 2, height / 2, depth / 2])
+
+    # Then rotate the vertices according to the given rotation
+    for i in range(8):
+        vertices[i] = rotate_point(vertices[i], mass_center, theta)
+        vertices[i] = rotate_point(vertices[i], mass_center, psi)
+        vertices[i] = rotate_point(vertices[i], mass_center, phi)
+
+    return vertices
+
+
 
 # rotate a point in response to a reference point
 def rotate_point(point, ref_point, theta):
@@ -156,6 +195,91 @@ def rotate_point(point, ref_point, theta):
 # check if two squares intersect
 
 # check if two squares intersect
+def get_edges(vertices):
+    """
+        Generate edges from vertices of a cuboid
+
+        Parameters:
+            vertices (np.array): 8x3 array of the vertices of the cuboid
+
+        Returns:
+            list: List of edges defined by vertex indices
+    """
+    edges = []
+    edges_set = set()
+    for i in range(8):
+        for j in range(i + 1, 8):
+            edge = [i, j]
+            edge.sort()
+            edge_tuple = tuple(edge)
+            if edge_tuple not in edges_set:
+                edges.append(edge)
+                edges_set.add(edge_tuple)
+    return edges
+
+def check_if_intersect3(cuboid1, cuboid2):
+    """
+        Check if two 3D cuboids intersect
+
+        Parameters:
+            cuboid1 (np.array): 8x3 array of the vertices of the first cuboid
+            cuboid2 (np.array): 8x3 array of the vertices of the second cuboid
+
+        Returns:
+            bool: True if the two cuboids intersect, False otherwise
+    """
+    edges1 = get_edges(cuboid1)
+    edges2 = get_edges(cuboid2)
+    for edge1 in edges1:
+        for edge2 in edges2:
+            if check_intersect(cuboid1[edge1[0]], cuboid1[edge1[1]], cuboid2[edge2[0]], cuboid2[edge2[1]]):
+                return True
+    return False
+
+def check_intersect(p1, q1, p2, q2):
+    """
+        Check if two line segments intersect
+
+        Parameters:
+            p1 (np.array): starting point of the first line segment
+            q1 (np.array): ending point of the first line segment
+            p2 (np.array): starting point of the second line segment
+            q2 (np.array): ending point of the second line segment
+
+        Returns:
+            bool: True if the two line segments intersect, False otherwise
+    """
+    def orientation(p, q, r):
+        val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+        if val == 0:
+            return 0
+        return 1 if val > 0 else 2
+
+    def on_segment(p, q, r):
+        return q[0] <= max(p[0], r[0]) and q[0] >= min(p[0], r[0]) and q[1] <= max(p[1], r[1]) and q[1] >= min(p[1], r[1])
+
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+
+    if o1 != o2 and o3 != o4:
+        return True
+
+    if o1 == 0 and on_segment(p1, p2, q1):
+        return True
+
+    if o2 == 0 and on_segment(p1, q2, q1):
+        return True
+
+    if o3 == 0 and on_segment(p2, p1, q2):
+        return True
+
+    if o4 == 0 and on_segment(p2, q1, q2):
+        return True
+
+    return False
+
 
 
 def check_if_intersect(square1, square2):
@@ -266,5 +390,30 @@ def check_intersection(data, square):
         square2 = create_square2(data[i])
         # check if they intersect
         if check_if_intersect2(square, square2):
+            return True
+    return False
+
+def check_intersection_3d(data, cuboid):
+    """
+        Check if a cuboid intersects with any of the cuboids in data
+
+        Parameters:
+            data (list): list of np.array of the vertices of the cuboids
+            x (float): x coordinate of the center of the cuboid
+            y (float): y coordinate of the center of the cuboid
+            size (float): size of the cuboid
+            rotation (float): rotation of the cuboid in degrees
+
+        Returns:
+            bool: True if the cuboid intersects with any of the cuboids in data
+    """
+    # create a cuboid
+    cuboid = create_cuboid(cuboid)
+    # check if it intersects with any of the cuboids in the data
+    for i in range(data.__len__()):
+        # create a cuboid
+        cuboid2 = create_cuboid(data[i])
+        # check if they intersect
+        if check_if_intersect3(cuboid, cuboid2):
             return True
     return False
--- src/utils/data.py
+++ src/utils/data.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random
+import src.geometry as geo
 
 def loadData(numberOfData):
     """
@@ -25,4 +27,93 @@ def loadData(numberOfData):
     print("Data loaded.")
 
     return data, datapoints
+
+def create_data_3d(numberOfData, x0, y0, z0, width, height, depth, theta, psi, phi, cube=True, axis_aligned=True):
+    """
+        Create numberOfData data
+    
+        Parameters:
+            numberOfData (int): number of data to load
+            
+        Returns:
+            data (np.array): data
+
+        Important:
+            The data are in the following format:
+            [x0, y0, z0, w, h, d, theta, phi] where:
+            x0, y0: coordinates of the center of the cuboid
+            w, h, z: width, height and depth of the cuboid
+            theta: rotation angle of the square in rad
+            phi: second
+    """
+    data = []
+    cnt = 0 #Keep track of cuboids created so far
+
+    #Maximum number of collisions allowed - If reached, the generation of cuboids stops
+    maximum_num_of_collisions = 0.05 * numberOfData
+    num_of_collisions = 0
+
+    while cnt != numberOfData:
+        #Store the results for every 500 cuboids created, starting from 1.000 and ending to 10.000
+        if 1000 <= cnt <= 10000 and (cnt - 1000) % 500 == 0:
+            print("reached ", cnt, " squares!")
+            np.save(f"./data_v2/10000sq/{cnt}sq_1_4.npy", data)
+
+        print("creating square ", cnt, "\n")
+        #Create the square with random center of mass
+        x0 = random.choice(x0)
+        y0 = random.choice(y0)
+        z0 = random.choice(z0)
+
+        #Check if the cuboid is actually a cube or not. Shape it accordingly
+        if(cube):
+            width = height = depth = random.choice(width)
+        else:
+            width = random.choice(width)
+            height = random.choice(height)
+            depth = random.choice(depth)
+
+        if(axis_aligned):
+            theta = psi = phi = 0
+        else:
+            theta = random.choice(theta)
+            psi = random.choice(psi)
+            phi = random.choice(phi)
+
+        current_cuboid = np.array([x0, y0, z0, width, height, depth, theta, psi, phi])
+        #Check if the current cuboid intersects with any of the cuboids in the data
+        if not geo.check_intersection(data, current_cuboid):
+            data.append(current_cuboid)
+            cnt += 1
+            num_of_collisions = 0
+        else:
+            num_of_collisions += 1
+            print(num_of_collisions)
+            #Terminate if maximum number of collisions reached
+            if num_of_collisions == maximum_num_of_collisions:
+                print(
+                    "max number of collisions reached! dataset creation terminates!\n"
+                )
+                break
+    return data
+
+
+def loadData_3d(numberOfData):
+    """
+        Load data from data folder
+    
+        Parameters:
+            numberOfData (int): number of data to load
+            
+        Returns:
+            data (np.array): data
+
+        Important:
+            The data must be in the following format:
+            [x0, y0, z0, w, h, d, theta, phi] where:
+            x0, y0: coordinates of the center of the cuboid
+            w, h, z: width, height and depth of the cuboid
+            theta: rotation angle of the square in rad
+            phi: second
+    """
     
