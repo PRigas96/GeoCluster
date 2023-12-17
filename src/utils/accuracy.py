c99415d4fef9f40e2986ceeef56a194a7ba28a4d
Thomas Pappas <getekid@gmail.com>
2023-12-17 15:56:59 +0200

    Adds number of queries per layer in the accuracy measurement

--- src/utils/accuracy.py
+++ src/utils/accuracy.py
@@ -16,12 +16,14 @@ def random_queries(ktree, n=300, times=4, k=1):
 
         # Run each query and store the number of correct predictions per layer.
         correct_predictions_per_layer = np.zeros(height - 1)
+        n_queries_per_layer = np.zeros(height - 1)
         for random_point in random_points:
             predictions_per_layer = ktree.query_verbose(random_point)["predictions per layer"]
             k_nearest_neighbors = ktree.root.query(random_point, k=k)
 
             is_correct = True
             for i, pred in enumerate(predictions_per_layer):
+                n_queries_per_layer[i] += 1
                 if k == 1 and not np.array_equal(pred, k_nearest_neighbors):
                     is_correct = False
                     break
@@ -35,6 +37,8 @@ def random_queries(ktree, n=300, times=4, k=1):
 
         accuracy_per_layer = correct_predictions_per_layer / n * 100
         mean_acc_per_layer += accuracy_per_layer
+        print("The number of queries per layer are:")
+        print(n_queries_per_layer)
         print(f"The percentage of correct predictions per layer is:")
         print(accuracy_per_layer)
 
@@ -54,12 +58,14 @@ def serialised_queries(ktree, n=500, k=1):
 
     # Run each query and store the number of correct predictions per layer.
     correct_predictions_per_layer = np.zeros(height - 1)
+    n_queries_per_layer = np.zeros(height - 1)
     for serialised_point in serialised_points:
         predictions_per_layer = ktree.query_verbose(serialised_point)["predictions per layer"]
         k_nearest_neighbors = ktree.root.query(serialised_point, k=k)
 
         is_correct = True
         for i, pred in enumerate(predictions_per_layer):
+            n_queries_per_layer[i] += 1
             if k == 1 and not np.array_equal(pred, k_nearest_neighbors):
                 is_correct = False
                 break
@@ -72,5 +78,7 @@ def serialised_queries(ktree, n=500, k=1):
             correct_predictions_per_layer[i] += int(is_correct)
 
     accuracy_per_layer = correct_predictions_per_layer / n * 100
+    print("The number of queries per layer are:")
+    print(n_queries_per_layer)
     print(f"The percentage of correct predictions per layer is: ")
     print(accuracy_per_layer)
